c7710f85ade98897f1074ef72cba293c2f0b8fba
Sankalp Sharma <sankalpshrm1111@gmail.com>
2025-03-10 18:10:34 +0530

    complete auth

--- videoTube/server/src/controllers/user.controllers.js
+++ videoTube/server/src/controllers/user.controllers.js
@@ -106,9 +106,64 @@ const loginUser = asyncHandler(async(req,res)=>{
 
 })
 const logoutUser = asyncHandler(async(req,res)=>{
-    
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset:{
+                refreshToken:1  //this remove field from the document
+            }
+        },
+        {
+            new:true
+        }
+
+    )
+    const options={
+        httpsOnly:true,
+        secure:true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new apiResponse(200,{},"User logged out"))
+})
+
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+    if(!incomingRefreshToken){
+        throw new apiError(401,"UNauthorised request")
+    }
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id)
+        if(!user){
+            throw new apiError(401,"Invalid refresh token")
+        }
+        if(incomingRefreshToken !== user.refreshToken){
+            throw new apiError(401,"refresh token is expired or used")
+        }
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+        const {accessToken,refreshToken} = generateAccessAndRefreshToken(user._id)
+
+        res
+        .status(200)
+        .cookie("refreshToken",refreshToken,options)
+        .cookie("accessToken",accessToken,options)
+        .json(
+            new apiResponse(200,{accessToken,refreshToken:newrefreshToken},"accessToken refresshed")
+        )
+
+    } catch (error) {
+        throw new apiError(401,error?.message || "Invalid refresh Token")
+    }
 
 })
+
 export{
     registerUser,
     loginUser,
