e8bb66a15e9fc4e9d75ee42e02830bdafce5d3cb
Sankalp Sharma <sankalpshrm1111@gmail.com>
2025-03-11 19:34:59 +0530

    server setup of the blog app is done

---
+++ blogApp/server/src/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import { User } from "../models/user.model.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const registerUser = asyncHandler(async(req,res)=>{
+    const {username,email,password}=req.body
+    if([username,email,password].some((fields)=> fields.trim === "")){
+        throw new apiError(500,"All fields are required")
+    }
+    
+}) 
+
+export{
+    registerUser
+}
---
+++ blogApp/server/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    coverImage:{
+        type:String
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true})
+
+export const User = mongoose.model("User",userSchema)
