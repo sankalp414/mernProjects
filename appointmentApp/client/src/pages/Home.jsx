2d8630917e0c5b312f694632379c2448d0e8dca1
Sankalp Sharma <sankalpshrm1111@gmail.com>
2025-03-15 13:44:01 +0530

    homePage

--- appointmentApp/client/src/pages/Home.jsx
+++ appointmentApp/client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../components/Header.jsx'
+import SpecialityMenu from '../components/SpecialityMenu.jsx'
+import TopDoctors from '../components/TopDoctors.jsx'
 
 const Home = () => {
   return (
     <div>
-      HomePage
+     <Header/>
+     <SpecialityMenu/>
+     <TopDoctors/>
     </div>
   )
 }
