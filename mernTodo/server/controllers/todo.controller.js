23ba034ccbfb30db06a0088477ef7b319f5e4d9c
Sankalp Sharma <sankalpshrm1111@gmail.com>
2025-08-21 12:55:35 +0530

    todo controller completed

--- mernTodo/server/controllers/todo.controller.js
+++ mernTodo/server/controllers/todo.controller.js
@@ -57,11 +57,67 @@ const deleteTodo = asyncHandler(async(req,res)=>{
     return res.status(200).json(new apiResponse(200,{},"Todo deleted successfully"))
 })
 
+const getAllTodo = asyncHandler(async(req,res)=>{
+    const {query,complete} = req.query
+
+    const todos = await Todo.aggregate([
+        {
+            $match:
+                query?.length >0    ?
+                {
+                    title:{
+                        $regex:query.trim(),
+                        $options: "i"
+                    }
+                }            
+            :{}
+        },
+        {
+            $match: complete
+            ? {
+                isComplete: JSON.parse(complete)
+            }:{}
+        },
+        {
+            updatedAt: -1
+        }
+    ])
+    return res
+    .status(200)
+    .json(new apiResponse(200,todos,"All todos fetched successfully"))
+})
 
 
+const toggleTodoStatusDone = asyncHandler(async (req,res) => {
+
+    const {todoId} = req.params
+    const todo = await Todo.findById(todoId)
+    if(!todo){
+        new apiError(400,"Todo does not exist")
+    }
+
+    todo.isComplete = !todo.isComplete
+
+    await todo.save({validateBeforeSave:false})
+    
+    res.status(200).json(new apiResponse(200,todo,"Todo toggled successfully"))
+})
+
+const getTodoById = asyncHandler(async (req,res) => {
+    const {todoId} = req.params
+    const todo = await Todo.findById(todoId)
+    if(!todo){
+        throw new apiError(404,"Todo does not exist")
+    }
+    res.status(200).json(new apiResponse(200,todo,"fetched todo by id"))
+    
+})
 
 export{
     createTodo,
     updateTodo,
-    deleteTodo
+    deleteTodo,
+    getAllTodo,
+    toggleTodoStatusDone,
+    getTodoById
 }
